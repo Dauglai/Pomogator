5d30511e6cf14a9eb430881caff1d754cdfdef8b
Dauglai <danil.samartsev.04@gmail.com>
2024-04-18 22:32:19 +0500

    Google Api oauth2

--- oauth/urls.py
+++ oauth/urls.py
@@ -1,15 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
 from .endpoint import views, auth_views
-from oauth.raw.apis import (
-    GoogleLoginApi,
-    GoogleLoginRedirectApi,
-)
+
+login_urlpatterns = [
+    path(
+        "raw/",
+        include(("oauth.raw.urls", "login-raw")),
+    ),
+    path(
+        "sdk/",
+        include(("oauth.sdk.urls", "login-sdk")),
+    ),
+
+]
 
 urlpatterns = [
-    path("callback/", GoogleLoginApi.as_view(), name="callback-raw"),
-    path("redirect/", GoogleLoginRedirectApi.as_view(), name="redirect-raw"),
-    path('', auth_views.google_login),
-    path('me/', views.UserView.as_view({'get': 'retrieve', 'put': 'update'})),
-    path('google/', auth_views.google_auth)
+    path("login/", include(login_urlpatterns)),
+    #path('', auth_views.google_login),
+   # path('me/', views.UserView.as_view({'get': 'retrieve', 'put': 'update'})),
+    #path('google/', auth_views.google_auth),
 ]
