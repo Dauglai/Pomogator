0d299d3bf5da24863d22ff9ca617256521134844
Dauglai <danil.samartsev.04@gmail.com>
2024-05-12 18:32:49 +0500

    Google Api Files Delete

--- api/views.py
+++ api/views.py
@@ -1,9 +1,11 @@
+from django.shortcuts import get_object_or_404
+from googleapiclient.discovery import build
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import TextFileSerializer
+from .serializers import CreateFileSerializer, FileSerializer
 from .models import File
-from .utils import create_google_doc, create_google_sheet, create_google_pres, create_google_forms
+from .utils import create_google_file, get_credentials
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
@@ -16,33 +18,40 @@ file_types = {
     "forms": "https://docs.google.com/forms/d/"
 }
 
-class CreateGoogleDocView(APIView):
+class GoogleFilesView(APIView):
 
     def get(self, request):
         txt = File.objects.all()
-        return Response({'posts': TextFileSerializer(txt, many=True).data})
+        return Response(FileSerializer(txt, many=True).data)
 
     def post(self, request):
-        serializer = TextFileSerializer(data=request.data)
+        serializer = CreateFileSerializer(data=request.data)
         if serializer.is_valid():
             name = serializer.validated_data['name']
             data = serializer.validated_data['data']
             id = serializer.validated_data['event_id']
             type = serializer.validated_data['type']
             try:
-                fileId = ""
-                if type == "document":
-                    fileId = create_google_doc(data, name, id)
-                if type == "sheet":
-                    fileId = create_google_sheet(data, name, id)
-                if type == "presentation":
-                    fileId = create_google_pres(data, name, id)
-                if type == "forms":
-                    fileId = create_google_forms(data, name, id)
                 if type == "":
                     return Response({"error": f"Указан неправильный тип документа"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+                fileId = create_google_file(name, data, type, id)
                 return Response({'message': 'Документ успешно создан.', 'link': f'{file_types[type]}{fileId}'}, status=status.HTTP_201_CREATED)
             except Exception as e:
                 return Response({"error": f"{e}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class GoogleFilesDetail(APIView):
+    def get(self, request, pk):
+        file = get_object_or_404(File, pk=pk)
+        serializer = FileSerializer(file)
+        return Response(serializer.data)
+
+
+    def delete(self, request, pk):
+        file = get_object_or_404(File, pk=pk)
+        service = build('drive', 'v3', credentials=get_credentials())
+        service.files().delete(fileId=file.file_id).execute()
+        file.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- event/admin.py
+++ event/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from .models import Event, Project, Task, Status, Type_Link, Link
+from .models import Event, Project, Task, Status
 
 admin.site.register(Event)
 admin.site.register(Project)
 admin.site.register(Task)
 admin.site.register(Status)
-admin.site.register(Type_Link)
-admin.site.register(Link)
